5cafd4f2b10c2737c81a8b5e13915d5ae43d45ac
gokulakrishnaa <gokulakrishnaapsivam@gmail.com>
2021-12-05 16:48:41 +0530

    Url Login

--- users.js
+++ users.js
@@ -27,4 +27,27 @@ router.route("/signup").post(async (request, response) => {
   response.send(result);
 });
 
+router.route("/login").post(async (request, response) => {
+  const { email, password } = request.body;
+  const userFromDB = await getUserByMail(email);
+  if (!userFromDB) {
+    response.status(400).send({ message: "Invalid Credentials" });
+    return;
+  }
+
+  if (password.length < 8) {
+    response.status(401).send({ message: "Password must be longer" });
+    return;
+  }
+
+  const storedPassword = userFromDB.password;
+  const isPasswordMatch = await bcrypt.compare(password, storedPassword);
+  if (isPasswordMatch) {
+    response.send({ message: "Login Successful" });
+  } else {
+    response.status(401).send({ message: "Invalid Credentials" });
+  }
+  response.send(result);
+});
+
 export const userRouter = router;
