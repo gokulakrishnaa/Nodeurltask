9203fa4294824ccaa108f08ba98756b366e433dc
gokulakrishnaa <gokulakrishnaapsivam@gmail.com>
2021-12-11 21:55:38 +0530

    fpass

--- fpass.js
+++ fpass.js
@@ -27,7 +27,7 @@ router.route("/forgot-password").post(async (req, res) => {
       from: process.env.USER_MAIL,
       to: `${email}`,
       subject: "Hello from Node App",
-      html: `Click <a href="https://nodeurlapp.herokuapp.com/fpass/reset-password/${userFromDB._id}/${token}">here</a> to confirm your registration`,
+      html: `Click <a href="https://nodeurlapp.herokuapp.com/fpass/reset-password/${userFromDB._id}/${token}">here</a> for resetting your password`,
     };
 
     transport.sendMail(mailOptions, function (err, info) {
